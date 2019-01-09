613fc25adb454607dd25527895f75e0069fa598e
fr3d100 <f.bonnand@gmail.com>
2019-01-09 08:41:03 +0100

    Commit du 2.1.3

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,7 +1,22 @@
-def say_hello
+# Gestion de la demande du prénom de l'utilisateur
+def ask_first_name
 	p "Quel est ton nom ?"
-	p "Ah, bonjour #{gets.chomp} !" 
+	first_name = gets.chomp
+	return first_name
 end
 
-say_hello
+# Gestion de l'affichage de la réponse
+def say_hello (firstname)
+	p "Ah, bonjour #{firstname} !" 
+end
+
+# Définition de la méthode perfom
+def perform
+	firstname = ask_first_name
+	say_hello(firstname)
+end
+
+# Execution du perform
+perform
+
 
